b952c05abd251e63cdc674333b177337d74f1096
Derni300 <alexou.debroissia@gmail.com>
2021-12-03 09:25:31 +0100

    Update

---
+++ src/YokaiGame.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+import java.util.Stack;
+
+public class YokaiGame {
+    private Player[] players;
+    public Cell[][] board = new Cell[6][6];
+
+    public void play() {
+        createPlayers();
+        initialiseBoard();
+        printBoard();
+    }
+
+    private void createPlayers() {
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Veuillez entrer le nom des joueurs");
+        players = new Player[2];
+        players[0] = new Player("Bob");
+        players[1] = new Player("Fanta");
+    }
+
+    private void shuffle(char[] cardList) {
+        Random random = new Random();
+        for (int i = 0; i < cardList.length; i++) {
+            int r = random.nextInt(cardList.length);
+            char temp = cardList[i];
+            cardList[i] = cardList[r];
+            cardList[r] = temp;
+        }
+    }
+
+    private void initialiseBoard() {
+        char[] cardList = {'R', 'R', 'R', 'R', 'B', 'B', 'B', 'B', 'P', 'P', 'P', 'P', 'G', 'G', 'G', 'G'};
+        shuffle(cardList);
+        //System.out.println(cardList);
+        int count = 0;
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                Position positionCard = new Position(i, j);
+                YokaiCard card = new YokaiCard(cardList[count], positionCard);
+                board[i][j] = new Cell(card, positionCard);
+                count += 1;
+            }
+        }
+
+        int playerNumber = 2;
+
+        Stack<String> colorStack = new Stack<String>();
+        String[] colorList = {"R","B","P","G"};
+
+        switch (playerNumber) {
+            case 2 :
+                String[] clueList = {};
+        }
+
+        Stack<String> clueStack = new Stack<String>();
+
+        for (int i = 0; i <= 7; i++){
+
+        }
+    }
+
+    private void printBoard() {
+        System.out.println();
+        System.out.println("+  -  -  -  -  -  -  +");
+        for (int i = 0; i < 6; i++) {
+            System.out.print("|  ");
+            for (int j = 0; j < 6; j++) {
+                if (board[i][j] == null) {
+                    System.out.print("¤  ");
+                } else {
+                    YokaiCard cardName = board[i][j].getCard();
+                    System.out.print(cardName.getName() + "  ");
+                }
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println("+  -  -  -  -  -  -  +");
+    }
+}
